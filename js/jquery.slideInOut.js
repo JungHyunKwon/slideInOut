c9fb12122ef0cd2c5c6b3262ec0cf712ad65ef0d
JungHyunKwon <32188481+JungHyunKwon@users.noreply.github.com>
2018-10-28 13:07:17 +0900

    published

--- js/jquery.slideInOut.js
+++ js/jquery.slideInOut.js
@@ -3,28 +3,28 @@
  * @version 1.0.0
  */
 try {
+	'use strict';
+
 	(function($) {
-		'use strict';
-
-		/**
-		 * @name 함수 생성
-		 * @since 2018-09-18
-		 * @param {string} type
-		 * @return {object}
-		 */
-		function genFx(type) {
-			var result = {};
-
-			for(var i = 0, propertyLength = property.length; i < propertyLength; i++) {
-				result[property[i]] = type;
-			}
-
-			return result;
-		}
-
 		//제이쿼리가 함수일 때
 		if(typeof $ === 'function') {
 			var property = ['width', 'marginRight', 'marginLeft', 'paddingRight', 'paddingLeft'];
+
+			/**
+			 * @name 함수 생성
+			 * @since 2018-09-18
+			 * @param {string} type
+			 * @return {object}
+			 */
+			function genFx(type) {
+				var result = {};
+
+				for(var i = 0, propertyLength = property.length; i < propertyLength; i++) {
+					result[property[i]] = type;
+				}
+
+				return result;
+			}
 
 			$.each({
 				slideIn : genFx('show'),
