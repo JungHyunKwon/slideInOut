89ac0651b78802488df0d52992b0dea7bea61213
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:15:27 +0900

    published

--- js/jquery.slideLrx.js
+++ js/jquery.slideLrx.js
@@ -2,43 +2,34 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
+(function($) {
 	'use strict';
 
-	(function($) {
-		//제이쿼리가 함수일 때
-		if(typeof $ === 'function') {
-			var _property = ['width', 'marginRight', 'marginLeft', 'paddingRight', 'paddingLeft'];
+	var _property = ['width', 'marginRight', 'marginLeft', 'paddingRight', 'paddingLeft'];
 
-			/**
-			 * @name 함수 생성
-			 * @since 2018-09-18
-			 * @param {string} type
-			 * @return {object}
-			 */
-			function _genFx(type) {
-				var result = {};
+	/**
+	 * @name 함수 생성
+	 * @since 2018-09-18
+	 * @param {string} type
+	 * @return {object}
+	 */
+	function _genFx(type) {
+		var result = {};
 
-				for(var i = 0, propertyLength = _property.length; i < propertyLength; i++) {
-					result[_property[i]] = type;
-				}
-
-				return result;
-			}
-
-			$.each({
-				slideLeft : _genFx('hide'),
-				slideRight : _genFx('show'),
-				slideX : _genFx('toggle')
-			}, function(name, value) {
-				$.fn[name] = function(speed, easing, callback) {
-					return this.animate(value, speed, easing, callback);
-				};
-			});
-		}else{
-			throw '제이쿼리가 없습니다.';
+		for(var i = 0, propertyLength = _property.length; i < propertyLength; i++) {
+			result[_property[i]] = type;
 		}
-	})(window.jQuery);
-}catch(e) {
-	console.error(e);
-}
+
+		return result;
+	}
+
+	$.each({
+		slideLeft : _genFx('hide'),
+		slideRight : _genFx('show'),
+		slideX : _genFx('toggle')
+	}, function(name, value) {
+		$.fn[name] = function(speed, easing, callback) {
+			return this.animate(value, speed, easing, callback);
+		};
+	});
+})(jQuery);
